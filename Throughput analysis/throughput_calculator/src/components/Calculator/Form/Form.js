a454df59c0b80ff2966b2decb83c47c305b02650
Szymon Gałecki <szymongalecki@MacBook-Air-Szymon.local>
2023-01-05 18:39:53 +0100

    Adding latex

--- Throughput analysis/throughput_calculator/src/components/Calculator/Form/Form.js
+++ Throughput analysis/throughput_calculator/src/components/Calculator/Form/Form.js
@@ -1,121 +1,106 @@
 import React from "react";
-import {
-  Paper,
-  TextField,
-  Grid,
-  Box,
-  InputAdornment,
-  Typography,
-  Button,
-} from "@mui/material";
+import { TextField, Grid, InputAdornment, Button } from "@mui/material";
 
 function Form() {
   return (
-    <Paper elevation={5}>
-      <Box sx={{ p: 4 }}>
-        <Typography sx={{ mb: 4 }} variant="h6">
-          5G NR Throughput Calculator
-        </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={6} sm={6}>
-            <TextField
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">(J)</InputAdornment>
-                ),
-              }}
-              label="Component Carriers"
-              type="number"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={6} sm={6}>
-            <TextField
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">(Q)</InputAdornment>
-                ),
-              }}
-              label="Modulation Order"
-              type="number"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={6} sm={6}>
-            <TextField
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">(V)</InputAdornment>
-                ),
-              }}
-              label="Number of layers"
-              type="number"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={6} sm={6}>
-            <TextField
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">(f)</InputAdornment>
-                ),
-              }}
-              label="Scaling Factor"
-              type="number"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={6} sm={6}>
-            <TextField
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">(mu)</InputAdornment>
-                ),
-              }}
-              label="NR numerology"
-              type="number"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={6} sm={6}>
-            <TextField
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">(N)</InputAdornment>
-                ),
-              }}
-              label="Number of allocated PRBs "
-              type="number"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">(OH)</InputAdornment>
-                ),
-              }}
-              fullWidth
-              label="Overhead"
-              type="number"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button fullWidth variant="contained">
-              Calculate
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
-    </Paper>
+    <Grid container spacing={2}>
+      <Grid item xs={6} sm={6}>
+        <TextField
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">(J)</InputAdornment>
+            ),
+          }}
+          label="Component Carriers"
+          type="number"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={6} sm={6}>
+        <TextField
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">(Q)</InputAdornment>
+            ),
+          }}
+          label="Modulation Order"
+          type="number"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={6} sm={6}>
+        <TextField
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">(V)</InputAdornment>
+            ),
+          }}
+          label="Number of layers"
+          type="number"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={6} sm={6}>
+        <TextField
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">(f)</InputAdornment>
+            ),
+          }}
+          label="Scaling Factor"
+          type="number"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={6} sm={6}>
+        <TextField
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">(mu)</InputAdornment>
+            ),
+          }}
+          label="NR numerology"
+          type="number"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={6} sm={6}>
+        <TextField
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">(N)</InputAdornment>
+            ),
+          }}
+          label="Number of allocated PRBs "
+          type="number"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">(OH)</InputAdornment>
+            ),
+          }}
+          fullWidth
+          label="Overhead"
+          type="number"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button fullWidth variant="contained">
+          Calculate
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
--- Throughput analysis/throughput_calculator/src/components/Calculator/Main.js
+++ Throughput analysis/throughput_calculator/src/components/Calculator/Main.js
@@ -1,15 +1,35 @@
 import React from "react";
 import Form from "./Form/Form";
-import { Grid, Paper, Box } from "@mui/material";
+import { Grid, Paper, Box, Typography, useMediaQuery } from "@mui/material";
+import { MathComponent } from "mathjax-react";
+
 function Main() {
+  const matches = useMediaQuery("(min-width:600px)");
+
   return (
     <Box sx={{ p: 4 }}>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={8}>
-          <Form />
+        <Grid item xs={12} sm={12} md={8}>
+          <Paper elevation={5}>
+            <Box sx={{ p: 4 }}>
+              <Typography sx={{ mb: 3 }} variant="h6">
+                5G NR Throughput Calculator
+              </Typography>
+              {matches && (
+                <Box sx={{ mb: 4 }}>
+                  <MathComponent
+                    tex={String.raw`10^{-6}\cdot\sum^{J}_{j=1}(v^{(j)}_{layers}{\cdot} Q^{(j)}_{m}{\cdot}f^{(j)}{\cdot}R_{max}{\cdot}\frac{N^{BW(j),\mu}_{PRB}{\cdot}12}{T^{\mu}_s}{\cdot}(1-OH^{(j)}))`}
+                  />
+                </Box>
+              )}
+              <Form />
+            </Box>
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper elevation={5}>Test</Paper>
+        <Grid item xs={12} sm={12} md={4}>
+          <Paper elevation={5}>
+            <Box sx={{ p: 4 }}></Box>
+          </Paper>
         </Grid>
       </Grid>
     </Box>
